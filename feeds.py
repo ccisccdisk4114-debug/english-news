39e5f3e7880af87d361ab106b141f21582a57cf4
ccisccdisk4114-debug <ccisccdisk4114@gmail.com>
2026-07-13 23:21:53 -0300

    Update feeds.py

---
+++ feeds.py
@@ -0,0 +1,5 @@
+RSS_FEEDS = {
+    "BBC": "https://feeds.bbci.co.uk/news/rss.xml",
+    "The Guardian": "https://www.theguardian.com/world/rss",
+    "ScienceDaily": "https://www.sciencedaily.com/rss/all.xml"
+}
