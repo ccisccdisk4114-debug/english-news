11f5a4282507f6c69b7ee4f53abad874133305be
ccisccdisk4114-debug <ccisccdisk4114@gmail.com>
2026-07-13 23:41:31 -0300

    Update discord_sender.py

--- discord_sender.py
+++ discord_sender.py
@@ -2,22 +2,53 @@ import requests
 import os
 
 
-def send_to_discord(message):
+def send_to_discord(news):
 
     webhook_url = os.environ["DISCORD_WEBHOOK"]
 
-    data = {
-        "content": message
+    embed = {
+        "title": f"📰 {news['title']}",
+        "url": news["link"],
+
+        "description": news["summary"],
+
+        "fields": [
+            {
+                "name": "🏢 Source",
+                "value": news["source"],
+                "inline": True
+            },
+            {
+                "name": "📅 Published",
+                "value": news["published"],
+                "inline": True
+            }
+        ],
+
+        "footer": {
+            "text": "🇬🇧 Daily English Reading • Improve your English every day"
+        },
+
+        "thumbnail": {
+            "url": news["image"]
+        }
     }
+
+
+    data = {
+        "embeds": [embed]
+    }
+
 
     response = requests.post(
         webhook_url,
         json=data
     )
 
+
     if response.status_code == 204:
-        print("Mensagem enviada ao Discord!")
+        print("Embed enviado ao Discord!")
 
     else:
-        print("Erro ao enviar:")
+        print("Erro ao enviar embed:")
         print(response.text)
