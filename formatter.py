148f56c799adee8954d4d74fbd134ed6b7e30508
ccisccdisk4114-debug <ccisccdisk4114@gmail.com>
2026-07-13 23:51:34 -0300

    Update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,1 +1,13 @@
+import re
 
+
+def clean_html(text):
+
+    if not text:
+        return ""
+
+    text = re.sub("<.*?>", "", text)
+
+    text = text.replace("&nbsp;", " ")
+
+    return text.strip()
