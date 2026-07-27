267396b7fc9ad7ee0dc7839f6628f983a9f58267
ccisccdisk4114-debug <ccisccdisk4114@gmail.com>
2026-07-27 02:53:32 -0300

    Update discord_sender.py

--- discord_sender.py
+++ discord_sender.py
@@ -3,9 +3,30 @@ import os
 from formatter import clean_html
 
 
+
+def get_news_role(source):
+
+    roles = {
+
+        "The Guardian": os.environ["MORNING_ROLE"],
+
+        "ScienceDaily": os.environ["AFTERNOON_ROLE"],
+
+        "BBC": os.environ["NIGHT_ROLE"]
+
+    }
+
+
+    return roles.get(source)
+
+
+
 def send_to_discord(news):
 
     webhook_url = os.environ["DISCORD_WEBHOOK"]
+
+
+    role = get_news_role(news["source"])
 
 
     embed = {
@@ -53,18 +74,37 @@ def send_to_discord(news):
         }
 
 
+
     data = {
 
+        # Marca o cargo
+        "content": f"<@&{role}>" if role else "",
+
+
         "embeds": [
+
             embed
-        ]
+
+        ],
+
+
+        # Permite que o webhook mencione cargos
+        "allowed_mentions": {
+
+            "roles": [role]
+
+        } if role else {}
 
     }
 
 
+
     response = requests.post(
+
         webhook_url,
+
         json=data
+
     )
 
 
